ebd8cab66b26fabd64683f786d046eb24fdc691a
quan <quan93909@gmail.com>
2025-09-06 12:39:33 +0700

    Hiển thị sản phẩm

--- js/product.js
+++ js/product.js
@@ -7,9 +7,27 @@ fetchApi("http://localhost:3000/products")
     let htmls = data.map(item => {
       return `
         <div class="product__item">
-           ${item.title}
+          <div class="product__image">
+              <img src="${item.thumbnail}" alt="${item.title}" />
+              <div class="product__percent">
+                  ${item.discountPercentage}%
+              </div>
+          </div>
+          <div class="product__content">
+              <h3 class="product__title">
+                  ${item.title}
+              </h3>
+              <div class="product__meta">
+                  <div class="product__price">
+                      ${item.price}
+                  </div>
+                  <div class="product__stock">
+                      Còn lại: ${item.stock} sp
+                  </div>
+              </div>
+          </div>
         </div>
-      `;
+        `;
     })
 
     products.innerHTML = htmls.join("");
